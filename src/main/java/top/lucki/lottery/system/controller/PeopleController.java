85479c493dab54ce3030e8f38a6a14535be2db8e
李威 <chall21@yeah.net>
2025-01-07 05:00:31 +0800

    抽奖开始后限制签到

--- src/main/java/top/lucki/lottery/system/controller/PeopleController.java
+++ src/main/java/top/lucki/lottery/system/controller/PeopleController.java
@@ -1,5 +1,6 @@
 package top.lucki.lottery.system.controller;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -7,6 +8,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import top.lucki.lottery.common.api.Result;
 import top.lucki.lottery.common.controller.BaseController;
@@ -34,6 +36,9 @@ public class PeopleController extends BaseController<People, IPeopleService> {
     private final IPeopleService peopleService;
     private final IProcessService processService;
     private final IRosterService rosterService;
+
+    @Value("spring.profiles.active")
+    private String activeProfile;
 
     /**
      * 分页列表查询
@@ -88,6 +93,18 @@ public class PeopleController extends BaseController<People, IPeopleService> {
      */
     @PostMapping(value = "/sign")
     public Result<?> sign(@RequestBody People people, HttpServletRequest req) {
+
+        // 不能早于 2025年1月10日 18:00
+        Date targetDate = DateUtil.parse("2025-01-10 18:00", "yyyy-MM-dd HH:mm");
+        if (DateUtil.date().before(targetDate) && StrUtil.equals(activeProfile, "prod")) {
+            return Result.error("时间还很早，不着急签到哦！");
+        }
+
+        int startProcess = processService.count();
+        if (startProcess > 0) {
+            return Result.error(501, "抽奖已经开始，不能再签到啦！");
+        }
+
         if (StrUtil.isEmpty(people.getPeopleName())) return Result.error("姓名不能为空！");
         People query = peopleService.lambdaQuery().eq(People::getPeopleName, people.getPeopleName()).one();
         Roster roster = rosterService.getByUsername(people.getPeopleName());
@@ -156,6 +173,12 @@ public class PeopleController extends BaseController<People, IPeopleService> {
     @GetMapping(value = "/queryById")
     public Result<?> queryById(@RequestParam(name = "id", required = true) String id) {
         People people = peopleService.getById(id);
+
+        int startProcess = processService.count();
+        if (startProcess > 0) {
+            people.addExtParam("startFlag", true);
+        }
+
         return Result.OK(people);
     }
 
